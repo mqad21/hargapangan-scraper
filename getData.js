1accb80938f42a09fd8ad83eb7470b46886489ef
Muhammad Qadri <mqad21@gmail.com>
2021-04-15 17:57:59 +0700

    First commit

---
+++ getData.js
@@ -0,0 +1,121 @@
+import puppeteer from "puppeteer";
+import cliProgress from "cli-progress";
+
+const getSelectOptions = async (page, selectId) => {
+  await page.waitForSelector("select#" + selectId);
+  return await page.evaluate((selectId) => {
+    const commoditySelectInput = document.getElementById(selectId);
+    return Object.values(commoditySelectInput.options).map((option) => {
+      return { text: option.text.trim(), value: option.value };
+    });
+  }, selectId);
+};
+
+export default (date) => {
+  const dateString =
+    date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+
+  console.log("Mengambil data tanggal: ", dateString);
+
+  const progressBar = new cliProgress.SingleBar(
+    {},
+    cliProgress.Presets.shades_classic
+  );
+  progressBar.start(100, 0);
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const browser = await puppeteer.launch({
+        headless: false,
+        args: ["--disable-setuid-sandbox"],
+        ignoreHTTPSErrors: true,
+      });
+
+      const url = "https://hargapangan.id";
+      const page = await browser.newPage();
+      await page.goto(url);
+
+      //   Mengambil select options.
+      const commodityId = "commodity_id";
+      const priceTypeId = "price_type_id";
+      const commodityOptions = await getSelectOptions(page, commodityId);
+      const priceTypeOptions = await getSelectOptions(page, priceTypeId);
+      resolve(priceTypeOptions);
+      //   Perulangan kombinasi options.
+      const dateId = "date";
+      let result = [];
+
+      await page.evaluate((dateId) => {
+        document.getElementById(dateId).removeAttribute("readonly");
+      }, dateId);
+
+      await page.focus("input#" + dateId);
+      await page.keyboard.down("Control");
+      await page.keyboard.press("A");
+      await page.keyboard.up("Control");
+      await page.keyboard.press("Backspace");
+      await page.type("input#" + dateId, dateString);
+
+      let i = 1;
+      const length = commodityOptions.length * priceTypeOptions.length;
+
+      for (const commodity of commodityOptions) {
+        await page.waitForSelector(
+          "#mapForm > div > div.control-group.col-md-1 > button > i.fa-arrow-down"
+        );
+        for (const priceType of priceTypeOptions) {
+          i++;
+          progressBar.update(Math.ceil((i / length) * 100));
+
+          await page.select("select#" + commodityId, commodity.value);
+          await page.select("select#" + priceTypeId, priceType.value);
+
+          await page.waitForTimeout(1000);
+
+          await page.click(
+            "#mapForm > div > div.control-group.col-md-1 > button"
+          );
+
+          await page.waitForSelector(
+            "#mapForm > div > div.control-group.col-md-1 > button > i.fa-arrow-down"
+          );
+
+          await page.waitForSelector(
+            'div.table-container[style*="opacity: 1"]'
+          );
+
+          const data = await page.evaluate(
+            (commodity, priceType, dateString) => {
+              const tableId = "DataTables_Table_0";
+              const rows = document.querySelectorAll(
+                "table#" + tableId + " tr"
+              );
+              return Array.from(rows, (row) => {
+                const columns = row.querySelectorAll("td");
+                return Array.from(columns, (column) => column.innerText);
+              })
+                .filter((item) => item.length)
+                .map((item) => {
+                  return {
+                    date: dateString,
+                    commodity,
+                    priceType,
+                    province: item[0],
+                    price: item[1],
+                  };
+                });
+            },
+            commodity.text,
+            priceType.text,
+            dateString
+          );
+          result.push(data);
+        }
+      }
+
+      resolve(result.flat());
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
---
+++ index.js
@@ -0,0 +1,32 @@
+import { createRequire } from "module";
+import getData from "./getData.js";
+import ObjectsToCsv from "objects-to-csv";
+
+const require = createRequire(import.meta.url);
+const prompt = require("prompt-sync")();
+
+let startDate = prompt("Masukkan Tanggal Awal (YYYY-MM-DD):");
+startDate = new Date(startDate);
+let endDate = prompt("Masukkan Tanggal Akhir (YYYY-MM-DD):");
+endDate = new Date(endDate);
+const fileName = prompt("Masukkan Nama File:");
+
+const diffTime = Math.abs(endDate - startDate) + 1;
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+const range = [...Array(diffDays).keys()];
+
+(async () => {
+  for (const i of range) {
+    try {
+      let date = new Date(startDate);
+      date.setDate(date.getDate() + i);
+      const data = await getData(date);
+      console.log("Menyimpan dalam csv...");
+      const csv = new ObjectsToCsv(data, { append: true });
+      await csv.toDisk("./" + fileName + ".csv");
+      console.log("Selesai :)");
+    } catch (e) {
+      console.error("Terdapat kesalahan: ", e);
+    }
+  }
+})();
